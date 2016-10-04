1427ae359174e0699bb3f7e66eae60bb6226938c
Ben Wilhelm <ben@doublebeamdesign.com>
2016-10-03 22:04:46 -0500

    Descend Store class from pyee.EventEmitter

--- flux/Store.py
+++ flux/Store.py
@@ -1,7 +1,12 @@
-class Store():
+from pyee import EventEmitter
+
+CHANGE_EVENT = 'change'
+
+class Store(EventEmitter):
     
     def __init__(self, dispatcher):
-        self.__data = {}
+        EventEmitter.__init__(self)
+        self.data = {}
         self.__actionHandlers = {}
         self.__watchHandlers = []
         self.dispatcherKey = dispatcher.register(self.handleDispatch)
@@ -9,7 +14,9 @@ class Store():
     def handleDispatch(self, action):
         for key in self.__actionHandlers:
             if key == action['type']:
-                self.__actionHandlers[key](action)
+                self.__actionHandlers[key](self, action)
+        print 'emitting ' + CHANGE_EVENT
+        self.emit(CHANGE_EVENT, self.data)
     
     def registerAction(self, key, callback):
         self.__actionHandlers[key] = callback
--- test.py
+++ test.py
@@ -5,22 +5,20 @@ d = Dispatcher.Dispatcher()
 s1 = Store.Store(d)
 s2 = Store.Store(d)
 
-def action_handler(action):
-    print "handled: '%s'"%(action['message'])
+def action_handler(store, action):
+    print 'handler'
+    store.data["value"] = action["value"]
 
-s1.registerAction('ACTION_ONE', action_handler)
-s2.registerAction('ACTION_TWO', action_handler)
+s1.registerAction('SET_VALUE', action_handler)
+# s2.registerAction('ACTION_TWO', action_handler)
 
+@s1.on(Store.CHANGE_EVENT)
+def on_change(data):
+    print 'change: ' + str(data['value'])
 
-action1 = {
-    'type': 'ACTION_ONE',
-    'message': 'this is a message one'
+action = {
+    'type': 'SET_VALUE',
+    'value': 4
 }
 
-action2 = {
-    'type': 'ACTION_TWO',
-    'message': 'this is a message two'
-}
-
-d.dispatch(action1)
-d.dispatch(action2)
+d.dispatch(action)
